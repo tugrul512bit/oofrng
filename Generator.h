30f336cd981b36fa6568e70e21f41438114603fe
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-03-09 00:04:13 +0300

    added limit parameter for float generation

--- Generator.h
+++ Generator.h
@@ -35,11 +35,14 @@ namespace oofrng
 			return rnd(alignedSeedBuf->seed,limit);
 		}
 
-
-
 		const float generate1Float()
 		{
 			return ((float)rnd(alignedSeedBuf->seed))*alignedSeedBuf->multiplier;
+		}
+
+		const float generate1Float(const float limit)
+		{
+			return ((float)rnd(alignedSeedBuf->seed))*alignedSeedBuf->multiplier*limit;
 		}
 
 		// fills array of length n with values between 0 and max(2^32-1)
@@ -87,6 +90,22 @@ namespace oofrng
 			for(size_t i=nL;i<n;i++)
 			{
 				out[i]=rnd(alignedSeedBuf->seed);
+			}
+		}
+
+		// generate [0,limit)
+		void generate(float * const __restrict__ out, const size_t n, const float limit)
+		{
+
+			const size_t nL = n-n%LANES;
+			for(size_t i=0;i<nL;i+=LANES)
+			{
+				rndL(alignedSeedBuf->ptrL,out+i,limit);
+			}
+
+			for(size_t i=nL;i<n;i++)
+			{
+				out[i]=rnd(alignedSeedBuf->seed,limit);
 			}
 		}
 
@@ -262,7 +281,45 @@ namespace oofrng
 			}
 		}
 
+		// generate [0,limit)
+		inline
+		void rndL(uint32_t * const __restrict__ seed, float * const __restrict__ out, const float limit)
+		{
 
+			const float mult = alignedSeedBuf->multiplier;
+			for(int i=0;i<LANES;i+=2)
+			{
+
+
+			   const uint32_t sd = seed[i];
+			   const uint32_t sd_ = seed[i+1];
+
+			   const uint32_t sd2 = (sd ^ 61) ^ (sd >> 16);
+			   const uint32_t sd2_ = (sd_ ^ 61) ^ (sd_ >> 16);
+
+			   const uint32_t sd3 = sd2*9;
+			   const uint32_t sd3_ = sd2_*9;
+
+			   const uint32_t sd4 = sd3 ^ (sd3 >> 4);
+			   const uint32_t sd4_ = sd3_ ^ (sd3_ >> 4);
+
+			   const uint32_t sd5 = sd4*0x27d4eb2d;
+			   const uint32_t sd5_ = sd4_*0x27d4eb2d;
+
+			   const uint32_t sd6 = sd5 ^ (sd5 >> 15);
+			   const uint32_t sd6_ = sd5_ ^ (sd5_ >> 15);
+
+			   const float sd7 = sd6*mult*limit;
+			   const float sd7_ = sd6_*mult*limit;
+
+			   out[i]=sd7;
+			   out[i+1]=sd7_;
+
+			   seed[i]=sd6;
+			   seed[i+1]=sd6_;
+
+			}
+		}
 	};
 
 }
