bca3ce0ba21257906d1d5e20243f649591a4430a
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2022-03-08 17:20:01 +0300

    Update Generator.h

--- Generator.h
+++ Generator.h
@@ -51,12 +51,13 @@ template<int LANES=64>
     	uint32_t seed;
 		const uint32_t rnd(uint32_t& seed)
 		{
-			       seed = (seed ^ 61) ^ (seed >> 16);
-	               seed *= 9;
-	               seed = seed ^ (seed >> 4);
-	               seed *= 0x27d4eb2d;
-	               seed = seed ^ (seed >> 15);
-	               return seed;
+			// Thomas Wang's invention
+			seed = (seed ^ 61) ^ (seed >> 16);
+			seed *= 9;
+			seed = seed ^ (seed >> 4);
+			seed *= 0x27d4eb2d;
+			seed = seed ^ (seed >> 15);
+			return seed;
 		}
 
 		inline
